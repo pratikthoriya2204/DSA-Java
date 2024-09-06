b4e4fbdec6852e3f0a205f725718bf15103bb04b
pratikthoriya2204 <pratikcollege123@gmail.com>
2024-09-06 14:57:32 +0530

    Sorting- Bubble Sort

---
+++ Sorting/bubble_SORT.java
@@ -0,0 +1,30 @@
+class bubble_SORT {
+    public static void printArray(int arr[]) {
+        for (int i = 0; i <= arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {6,5,9,3,1,2,4};
+
+        //time complexity = O(n^2)
+        // Bubble Sort
+        for (int i=0; i<arr.length-1;i++){
+            for (int j=0;j<arr.length-i-1;j++){
+                if(arr[j] > arr[j+1]){
+                    //SWAP
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+
+
+        printArray(arr);
+    }
+}
+
+// OUTPUT :---
